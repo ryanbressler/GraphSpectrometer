031dc86819cc4fdedc9d7b0d07fbe86d8c9556e5
Ryan Bressler <ryanbressler@gmail.com>
2012-10-23 12:09:19 -0600

    switch pw comp to use runpw.sh

--- runpwcomps.sh
+++ runpwcomps.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # usage ./runall.sh /titan/cancerregulome9/ITMI_PTB/public/analysis/ /titan/cancerregulome9/workspaces/users/rbressle/2012_09_18/blacklist.txt /titan/cancerregulome9/ITMI_PTB/public/analysis/layouts/
 export NGSPECCORES=4
+export NGSPECPLOTINGCORES=2
 export GSPEC=/titan/cancerregulome9/workspaces/users/rbressle/GraphSpectrometer
 
 #for FILE in /titan/cancerregulome3/TCGA/outputs/brca/brca.newMerge.01oct.hack.imp1_NCI.*.pwpv
@@ -8,7 +9,13 @@ export GSPEC=/titan/cancerregulome9/workspaces/users/rbressle/GraphSpectrometer
 #	grep GNAB $FILE > $(basename $FILE)
 #done
 
-ls *.pwpv | xargs -I FILE  bash ${GSPEC}/runpw.sh FILE .
+for PWFILE in *.pwpv
+do
+	seq .8 -.2 .2 | xargs --max-procs=${NGSPECCORES} -I CUTOFF  \
+	python ${GSPEC}/fiedler.py \
+	${PWFILE} CUTOFF
+done
 
-python ${GSPEC}/comparisonPlot.py "*.pwpv.json"
+seq .8 -.2 .2 | xargs --max-procs=${NGSPECPLOTINGCORES} -I CUTOFF  \
+python ${GSPEC}/comparisonPlot.py "*${CUTOFF}*${CUTOFF}*.pwpv.json"
 
