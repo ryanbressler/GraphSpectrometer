0c98d09c45f2c8d7649c05a4cd56aa4bb9e2bc90
Ryan Bressler <ryanbressler@gmail.com>
2012-10-26 16:56:07 -0600

    new dir structure

--- runrf.sh
+++ runrf.sh
@@ -16,21 +16,6 @@ python ${GSPEC}/parseRfPred.py ${TREES} CUTOFF
 echo FINDING HODGE RANK
 ls ${OUTDIR}/*.json | xargs --max-procs=${NGSPECPLOTINGCORES} -I FILE  \
 python ${GSPEC}/plotpredDecomp.py FILE
- 
-#!/usr/bin/env bash
-FMATRIX=$1
-BLACKLIST=$2
-OUTDIR=$3
 
-cd ${OUTDIR}
-echo RUNNING RANDOM FOREST WITH BLACKLIST
-$RFACE -I $FMATRIX \
--i N:CLIN:TermCategory:NB:::: -B ${BLACKLIST} -O ${OUTDIR}/rf.pred.w.12800.blacklisted.out -n 12800
-echo PARSING PREDICTOR AND FINDING FIEDLER VECTORS
-seq 0 1 60 | xargs --max-procs=${NGSPECCORES} -I CUTOFF  \
-python ${GSPEC}/parseRfPred.py ${OUTDIR}/rf.pred.w.12800.blacklisted.out CUTOFF
-echo FINDING HODGE RANK
-ls ${OUTDIR}/rf.pred.w.12800.blacklisted.out*.json | xargs --max-procs=${NGSPECPLOTINGCORES} -I FILE  \
-python ${GSPEC}/plotpredDecomp.py FILE
 
 
