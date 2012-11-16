6e3cea09a95ea0103b09b5502cd0e34f6b20f97d
Ryan Bressler <ryanbressler@gmail.com>
2012-11-16 15:57:52 -0700

    pruneing of leaves by feature

--- runrf_no_bl.sh
+++ runrf_no_bl.sh
@@ -12,7 +12,7 @@ if [ ! -e "$TREES" ];
 then
 	echo RUNNING RANDOM FOREST WITH BLACKLIST
 	$RFACE -I $FMATRIX \
-	-i N:CLIN:TermCategory:NB:::: -O ${TREES} -n 12800 -m 75 -a 1000
+	-S ${TREES} -i N:CLIN:TermCategory:NB:::: -n 12800 -m 75 -a 1000 -s 1
 fi
 
 
--- setvars.sh
+++ setvars.sh
@@ -3,7 +3,8 @@ export NGSPECPLOTINGCORES=2
 export RFACE=/titan/cancerregulome9/ITMI_PTB/bin/rf-ace-read-only/bin/rf-ace
 export GSPEC=/titan/cancerregulome9/ITMI_PTB/bin/GraphSpectrometer
 export LCOUNT=/titan/cancerregulome9/ITMI_PTB/bin/go/src/github.com/ryanbressler/CloudForest/leafcount/leafcount
-export BLACKLIST=/titan/cancerregulome9/ITMI_PTB/public/analysis/blacklists/blackListTemplate.txt
+export BLACKLIST=/titan/cancerregulome9/ITMI_PTB/public/analysis/blacklists/weightedBlacklist
+export RFACEOPTIONS=-i N:CLIN:TermCategory:NB:::: -n 12800 -m 75 -a 1000 -s 1
 
 
 
