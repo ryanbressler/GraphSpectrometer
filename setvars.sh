20c54716e9596c33949df2c53a32527f1ffd2fa8
Ryan Bressler <ryanbressler@gmail.com>
2013-02-05 21:27:04 -0700

    update blacklist

--- setvars.sh
+++ setvars.sh
@@ -3,7 +3,7 @@ export NGSPECPLOTINGCORES=2
 export RFACE=/titan/cancerregulome9/ITMI_PTB/bin/rf-ace-read-only/bin/rf-ace
 export GSPEC=/titan/cancerregulome9/ITMI_PTB/bin/GraphSpectrometer
 export LCOUNT=/titan/cancerregulome9/ITMI_PTB/bin/go/src/github.com/ryanbressler/CloudForest/leafcount/leafcount
-export BLACKLIST=/titan/cancerregulome9/ITMI_PTB/public/analysis/blacklists/blackListTemplate.txt 
+export BLACKLIST=/titan/cancerregulome9/ITMI_PTB/public/analysis/blacklists/mBlackListTemplate.txt 
 export RFACEOPTIONS="-i N:CLIN:TermCategory:NB:::: -n 12800 -m 100 -a 1000 -s 4 -e 8"
 
 
