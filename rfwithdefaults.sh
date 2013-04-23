9b6cf7343a9c98a4b71095048596ffe2c60f9b46
Ryan Bressler <ryanbressler@gmail.com>
2013-04-23 12:14:02 -0600

    syntax fix

--- rfwithdefaults.sh
+++ rfwithdefaults.sh
@@ -4,7 +4,7 @@ FMATRIX=$1
 OUTDIR=$(pwd)
 TARGET=B:CLIN:Preterm:NB::::
 NAME=$(basename $FMATRIX)
-MTRY=$(wc -l $FMATRIX) | awk 'END {print int(sqrt($1))}'
+MTRY=$(wc -l $FMATRIX) | awk 'END {print int(sqrt($1))}')
 
 TREES=${OUTDIR}/${NAME}.sf
 
