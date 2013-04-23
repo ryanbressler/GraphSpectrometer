c7be83d7fb4839195c95953987f72af4e1b1159d
Ryan Bressler <ryanbressler@gmail.com>
2013-04-23 15:19:22 -0600

    fix typo

--- mean.py
+++ mean.py
@@ -7,12 +7,12 @@ def main():
 	for line in sys.stdin:
 		vs = line.rstrip().split()
 		if True!=(vs[0] in count):
-			count[vs[0]]=0
+			count[vs[0]]=0.0
 			error[vs[0]]=0.0
-		count[vs[0]]+=1
+		count[vs[0]]+=1.0
 		error[vs[0]]+=float(vs[1])
 	for name,c in enumerate(count):
-		print "%s\t%s"%(name,string(error[name]/c))
+		print "%s\t%s"%(name,error[name]/c)
 
 if __name__ == '__main__':
 	main()
