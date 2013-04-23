e376372e7e1970900c95bb91567749e91b231211
Ryan Bressler <ryanbressler@gmail.com>
2013-04-23 15:09:19 -0600

    mean.py

---
+++ mean.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+
+def main():
+	count = {}
+	error = {}
+	for line in sys.stdin:
+		vs = line.rstrip().split()
+		if ! vs[0] in count:
+			count[vs[0]]=0
+			error[vs[0]]=0.0
+		count[vs[0]]+=1
+		error[vs[0]]+=float(vs[1])
+	for name,c in count:
+		print "%s\t%s"%(name,string(error[name]/c))
+
+if __name__ == '__main__':
+	main()
+	
