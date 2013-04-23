f1c52405a0f6542eae36148e16e0ffab52c46589
Ryan Bressler <ryanbressler@gmail.com>
2013-04-23 13:16:45 -0600

    adding error rate

--- errorrate.sh
+++ errorrate.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+#ls */folds/*/test*.fm | xargs -P 8 -I FILE bash /titan/cancerregulome9/ITMI_PTB/bin/GraphSpectrometer/errorrate.sh FILE
 TESTFILE=$1
 FORREST=${TESTFILE//test/train}.sf
 ERROR=$($GOPATH/bin/errorrate -fm ${TESTFILE} -rfpred ${FORREST})
-echo ${TESTFILE}\t${ERROR}
+echo ${TESTFILE}	${ERROR}
